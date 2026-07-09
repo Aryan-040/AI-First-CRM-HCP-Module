4d9ece38c8c705f0c6392168c87cb577a32dd61f
Aryan-040 <singharyan432002@gmail.com>
2026-07-09 13:59:29 +0530

    Fix CORS for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000" , "https://ai-first-crm-hcp-module-inky.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
